e4bf75657e808589be3446ed079870616e7c3913
smolphie <sofychen@g.ucla.edu>
2023-04-13 11:40:32 -0700

    basic skeleton for Game class

--- ctf/src/game.py
+++ ctf/src/game.py
@@ -3,11 +3,18 @@ import json
 
 grid_size = 10
 
-class State: # each state stores the positions, reward, action, and terminal status
-    def __init__(self, agent_position, obstacle_position, flag_position):
+# OUTPUT : trajectory - json file - a numpy array as the output 
+# have a function that changes the numpy array to a json file 
+
+# class should be called game 
+# for the initialization you have the state 
+# intialization should take care of the random state section 
+
+class Game: # each state stores the positions, reward, action, and terminal status
+    def __init__(self, agent_positions, obstacle_positions, flag_position, action_cost, goal_reward):
         self.state_dict = {
-            "a1": agent_position,
-            "o1": obstacle_position,
+            "a1": agent_positions,
+            "o1": obstacle_positions,
             "f1": flag_position
         }
         self.reward = { # default reward is 0
@@ -16,38 +23,48 @@ class State: # each state stores the positions, reward, action, and terminal sta
         self.action = { # default action is not moving
             'a1': 0
         }
+        self.action_cost = action_cost 
+        self.goal_reward = goal_reward
         self.terminal = False
+        
+
+    # just be inputing the state and return the action 
+    # policy random , random choice 
+    # returns the ones thats chosen 
 
     def policy_random(state): 
         agent_actions = []
         possible_moves = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0)] 
-        agent_position = state.state_dict['a1'] # take coord of agent from state dictionary
-        
-        for move in possible_moves: 
-            # calculate the new position of the agent by adding the move to the agent's current position
-            new_pos = (agent_position[0] + move[0], agent_position[1] + move[1])
-        
-            #check if the new position is outside the grid or collides with an obstacle
-            if (new_pos[0] < 0 or new_pos[0] >= grid_size or
-                new_pos[1] < 0 or new_pos[1] >= grid_size or 
-                new_pos == state.state_dict['o1']): 
-                # if the new position is invalid, skip this move 
-                continue
-            # if the new position is valid, add move to the agents_action list 
-            agent_actions.append(move)
-        # add an action to agent_actions for every agent in state
-        return agent_actions  # returns a new state from agents movement 
+        # agent_position = state.state_dict['a1'] # take coord of agent from state dictionary
+
+        # return a random action from the list of valid actions
+        return random.choice(possible_moves)
+
     
-    def transition(state, agent_action):
+    def transition(state, agent_actions):
         new_state = state.copy()
         #update all the agent coordinates in the state
+
+            
         return new_state
     
     def reward(state, action):
         # how is reward calculated? not sure what -action_cost means
+        # action cost should be dependent on what action you take 
+        # one exception , when agents gets to goal -> define where the flag is 
         return 0
+    
+    # checking weather the agent has captured the flag or gotten to the goal 
+    def is_terminal(state): 
+        terminal = False
+        for agent_pos in state.state_dict["a1"]:
+            if agent_pos == state.state_dict["f1"]:
+                terminal = true
+        return terminal 
+    # get to the flag, getting a winning bonus -
+
 
 # generates a random initial state for the envrioment 
-state = State((random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)),
+state = Game((random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)),
               (random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)),
               (random.randint(0, grid_size - 1), random.randint(0, grid_size - 1)))
