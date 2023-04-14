219e7929e6fd95ef761164aa964a006e145b99a7
James Zhang <zhang_james@icloud.com>
2023-04-14 14:24:16 -0700

    Changed directory for json file fetching
    Moved json file output to the parent directory

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const io = require("socket.io")(8080, {
 io.on("connection", socket => {
     console.log(socket.id);
     socket.on('state-to-server', () => {
-        const jsonData = require('./data.json'); // Load the data from data.json
+        const jsonData = require('../all_episode_trajectories.json'); // Load the data from data.json
         console.log('Loaded data from data.json:', jsonData); // Add this line
         const jsonString = JSON.stringify(jsonData); // Stringify the JSON data
         io.emit('state-to-client', jsonString); // Emit the stringified JSON data
