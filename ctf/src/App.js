cb20020ea9e44478df4f59d3aed3e8bae16f3e13
James Zhang <zhang_james@icloud.com>
2023-04-26 16:26:37 -0700

    Changes to app.js
    - Parsing new json file format is added
    - Given each state in the episode, it now renders the new coordinates once every second, allowing the agents to move

--- ctf/src/App.js
+++ ctf/src/App.js
@@ -8,8 +8,11 @@ const socket = io('http://localhost:8080');
 
 const App = () => {
 
-  const [agents, setAgents] = useState([]);
-  const [obstacles, setObstacles] = useState([]);
+  const [stateIndex, setStateIndex] = useState(0); // Keep track of the current state index
+  const [states, setStates] = useState([]); // Store all the states from the JSON file
+  const [agents, setAgents] = useState([]); // Store the agents for the current state
+  const [obstacles, setObstacles] = useState([]); // Store the obstacles for the current state
+  const [message, setMessage] = useState(null); // Store the message from the server
 
   useEffect(() => {
     // Use socket.emit to send a request to the server
@@ -21,16 +24,8 @@ const App = () => {
       const message = JSON.parse(jsonMessage);
       console.log('Received message:', message);
 
-      // Access the agent and obstacles data from the parsed message
-      const agents = message[0].agent;
-      const obstacles = message[0].obstacle;
-
-      // Convert obstacle array to an array of objects with row and col properties
-      const obstacleCoords = obstacles.map(o => ({ row: o.row, col: o.col }));
-
-      // Update the agents and obstacles state
-      setAgents(agents);
-      setObstacles(obstacleCoords);
+      // Update the message state with the parsed message
+      setMessage(message);
     });
 
     // Clean up the socket event listener when component unmounts
@@ -38,6 +33,36 @@ const App = () => {
       socket.off('send-state-to-client');
     }
   }, []);
+
+  useEffect(() => {
+    console.log("parsing states");
+
+    // If message is empty, return early
+    if (!message) {
+      return;
+    }
+    // Get the current state based on the stateIndex
+    const currentState = message[stateIndex];
+
+    // Access the agent and obstacles data from the current state
+    const agents = currentState[0].agent;
+    const obstacles = currentState[0].obstacle;
+
+    // Convert obstacle array to an array of objects with row and col properties
+    const obstacleCoords = obstacles.map(o => ({ row: o.row, col: o.col }));
+
+    // Update the agents and obstacles state
+    setAgents(agents);
+    setObstacles(obstacleCoords);
+
+    // Use setTimeout to update the stateIndex after one second, if it's not the last state
+    if (stateIndex < message.length - 1) {
+      const timeoutId = setTimeout(() => {
+        setStateIndex(stateIndex + 1);
+      }, 1000);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [stateIndex, message]);
 
   return (
     <div>
