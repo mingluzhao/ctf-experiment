13321d3fa319b747042cf1d240aa6c7c8d3b24e2
James Zhang <zhang_james@icloud.com>
2023-05-08 01:12:26 -0700

    Agent rotation added

--- ctf/src/Agent.js
+++ ctf/src/Agent.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Agent = ({ src, position }) => {
+const Agent = ({ src, position, direction }) => {
   const squareSize = 40;
   const halfSquareSize = squareSize / 2;
   const top = `calc(${position.top} - ${halfSquareSize}px)`;
   const left = `calc(${position.left} - ${halfSquareSize}px)`;
+  const transform = `rotate(${direction}deg)`;
   
   return (
-    <div style={{ position: 'absolute', top, left }}>
+    <div style={{ position: 'absolute', top, left, transform }}>
       <img src={src} style={{ height: `${squareSize-5}px`, width: `${squareSize-5}px`, objectFit: "contain", border: "none" }} />
     </div>
   );
@@ -20,6 +21,7 @@ Agent.propTypes = {
     top: PropTypes.string.isRequired,
     left: PropTypes.string.isRequired,
   }).isRequired,
+  direction: PropTypes.number.isRequired,
 };  
 
 export default Agent;
--- ctf/src/App.js
+++ ctf/src/App.js
@@ -1,61 +1,49 @@
 import React, { useState, useEffect } from 'react';
 import GameBoard from './GameBoard';
 import Agent from './Agent';
-import { io } from "socket.io-client"
-import agentImg from "./agent.png"
+import { io } from 'socket.io-client';
+import agentImg from './agent.png';
 
 const socket = io('http://localhost:8080');
 
 const App = () => {
-
-  const [stateIndex, setStateIndex] = useState(0); // Keep track of the current state index
-  //const [states, setStates] = useState([]); // Store all the states from the JSON file
-  const [agents, setAgents] = useState([]); // Store the agents for the current state
-  const [obstacles, setObstacles] = useState([]); // Store the obstacles for the current state
-  const [message, setMessage] = useState(null); // Store the message from the server
+  const [stateIndex, setStateIndex] = useState(0);
+  const [agents, setAgents] = useState([]);
+  const [obstacles, setObstacles] = useState([]);
+  const [message, setMessage] = useState(null);
 
   useEffect(() => {
-    // Use socket.emit to send a request to the server
     socket.emit('send-state-to-server');
 
-    // Listen for a message from the server
     socket.on('send-state-to-client', (jsonMessage) => {
-      // Parse the JSON message
       const message = JSON.parse(jsonMessage);
       console.log('Received message:', message);
 
-      // Update the message state with the parsed message
       setMessage(message[0]);
     });
 
-    // Clean up the socket event listener when component unmounts
     return () => {
       socket.off('send-state-to-client');
     }
   }, []);
 
   useEffect(() => {
-    console.log("parsing states");
+    console.log('Parsing states');
 
-    // If message is empty, return early
     if (!message) {
       return;
     }
-    // Get the current state based on the stateIndex
+
     const currentState = message[stateIndex];
 
-    // Access the agent and obstacles data from the current state
     const agents = currentState[0].agent;
     const obstacles = currentState[0].obstacle;
 
-    // Convert obstacle array to an array of objects with row and col properties
     const obstacleCoords = obstacles.map(o => ({ row: o.row, col: o.col }));
 
-    // Update the agents and obstacles state
     setAgents(agents);
     setObstacles(obstacleCoords);
 
-    // Use setTimeout to update the stateIndex after one second, if it's not the last state
     if (stateIndex < message.length - 1) {
       const timeoutId = setTimeout(() => {
         setStateIndex(stateIndex + 1);
@@ -64,28 +52,49 @@ const App = () => {
     }
   }, [stateIndex, message]);
 
+  useEffect(() => {
+    agents.forEach(agent => {
+      const agentImgElem = document.getElementById(agent.id);
+      if (agentImgElem) {
+        agentImgElem.style.transform = `rotate(${getAngle(agent.direction)}deg)`;
+      }
+    });
+  }, [agents]);
+
+  const getAngle = (direction) => {
+    switch (direction) {
+      case "up":
+        return 0;
+      case "down":
+        return 180;
+      case "left":
+        return -90;
+      case "right":
+        return 90;
+      default:
+        return 0;
+    }
+  }
+
   return (
     <div>
-      {console.log(agents)}
-      {console.log(obstacles)}
-      {/* Render GameBoard component only if agents and obstacles are not empty */}
       {agents.length > 0 && obstacles.length > 0 &&
         <GameBoard
-          numRows={10} // Set the number of rows for the game board
-          numCols={10} // Set the number of columns for the game board
-          obstacleCoords={obstacles} // Pass the obstacle coordinates to GameBoard
+          numRows={10}
+          numCols={10}
+          obstacleCoords={obstacles}
           agentCoords={agents}
         />
       }
-      {/* Render Agent components for each agent in agents */}
       {agents.map(agent => (
         <Agent
-          key={agent.id} // Assign a unique key to each Agent component
+          key={agent.id}
           src={agentImg}
           position={{
             top: `${40 * agent.row + 30}px`,
             left: `${40 * agent.col + 30}px`
           }}
+          direction={getAngle(agent.direction)}
         />
       ))}
     </div>
