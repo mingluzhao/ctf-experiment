a7ab61213a873b660e8cca12cecc2dfdf10f135a
Minglu Zhao <minglu.zhao@ucla.edu>
2023-09-06 19:09:28 -0400

    modify exp settings

--- python-server/constants.py
+++ python-server/constants.py
@@ -4,12 +4,16 @@ possible_moves = ["forward", "backward", "turn_r", "turn_l"]
 move_map = {'forward': 0, 'right': 1, 'backward': 2, 'left': 3}
 
 # ADJUSTABLE PARAMETERS
-save_toggle = False
-max_steps = 200
-max_round = 1
+save_toggle = True
+max_steps = 50
+max_round = 2
 full_visible = True
-num_obstacles = 8
-step_time = 0.5
+num_obstacles = 8 # if randomly generated
+step_time = 1
+
+obstacles_list = [[(2, 2), (2, 7), (7, 2), (7, 7)],
+                  [(2, 2), (3, 6), (7, 2), (7, 7)]
+                  ]
 
 init_state = {
             "agent": [
@@ -65,63 +69,81 @@ init_state = {
             ]
         }
 
-obstacle_maps = [[{
-                    "id": "o1",
-                    "row": 0,
-                    "col": 1
-                },
-                {
-                    "id": "o2",
-                    "row": 1,
-                    "col": 1
-                },
-                {
-                    "id": "o3",
-                    "row": 2,
-                    "col": 1
-                },
-                {
-                    "id": "o4",
-                    "row": 7,
-                    "col": 7
-                }],
-                [{
-                    "id": "o1",
-                    "row": 3,
-                    "col": 3
-                },
-                {
-                    "id": "o2",
-                    "row": 2,
-                    "col": 7
-                },
-                {
-                    "id": "o3",
-                    "row": 7,
-                    "col": 2
-                },
-                {
-                    "id": "o4",
-                    "row": 7,
-                    "col": 7
-                }],
-                [{
-                    "id": "o1",
-                    "row": 2,
-                    "col": 2
-                },
-                {
-                    "id": "o2",
-                    "row": 3,
-                    "col": 6
-                },
-                {
-                    "id": "o3",
-                    "row": 7,
-                    "col": 2
-                },
-                {
-                    "id": "o4",
-                    "row": 7,
-                    "col": 7
-                }]]
+
+def generate_obstacle_maps(position_list):
+    obstacle_maps = []
+
+    for positions in position_list:
+        map_list = []
+        for index, (row, col) in enumerate(positions, start=1):
+            obstacle = {
+                "id": f"o{index}",
+                "row": row,
+                "col": col
+            }
+            map_list.append(obstacle)
+        obstacle_maps.append(map_list)
+
+    return obstacle_maps
+
+obstacle_maps = generate_obstacle_maps(obstacles_list)
+# obstacle_maps = [[{
+#                     "id": "o1",
+#                     "row": 0,
+#                     "col": 1
+#                 },
+#                 {
+#                     "id": "o2",
+#                     "row": 1,
+#                     "col": 1
+#                 },
+#                 {
+#                     "id": "o3",
+#                     "row": 2,
+#                     "col": 1
+#                 },
+#                 {
+#                     "id": "o4",
+#                     "row": 7,
+#                     "col": 7
+#                 }],
+#                 [{
+#                     "id": "o1",
+#                     "row": 3,
+#                     "col": 3
+#                 },
+#                 {
+#                     "id": "o2",
+#                     "row": 2,
+#                     "col": 7
+#                 },
+#                 {
+#                     "id": "o3",
+#                     "row": 7,
+#                     "col": 2
+#                 },
+#                 {
+#                     "id": "o4",
+#                     "row": 7,
+#                     "col": 7
+#                 }],
+#                 [{
+#                     "id": "o1",
+#                     "row": 2,
+#                     "col": 2
+#                 },
+#                 {
+#                     "id": "o2",
+#                     "row": 3,
+#                     "col": 6
+#                 },
+#                 {
+#                     "id": "o3",
+#                     "row": 7,
+#                     "col": 2
+#                 },
+#                 {
+#                     "id": "o4",
+#                     "row": 7,
+#                     "col": 7
+#                 }]]
