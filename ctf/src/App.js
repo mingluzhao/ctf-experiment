d570b7bab3efb2f466731b7a3a90822db1a66ae1
Minglu Zhao <minglu.zhao@ucla.edu>
2023-09-06 19:47:14 -0400

    ip change

--- ctf/src/App.js
+++ ctf/src/App.js
@@ -3,8 +3,8 @@ import GameBoard from './GameBoard';
 import { io } from 'socket.io-client';
 
 
-//const socket = io('http://128.97.30.83:8080');
-const socket = io('http://localhost:8080');
+const socket = io('http://128.97.30.83:8080');
+// const socket = io('http://localhost:8080');
 
   // =======PREGAME DISPLAY=======
   function StartScreen({ onStart, onJoin, roomID }) {
